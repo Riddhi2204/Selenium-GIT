b858b7d975b1416386dc1efc0e7ab58197c66ac0
riddhi.gupta <riddhi.gupta@DDC2-L-B4L6GC2.dir.svc.accenture.com>
2018-07-18 11:14:06 +0530

    Second Commit

---
+++ Log4j/src/log4jPackage/test.java
@@ -0,0 +1,11 @@
+package log4jPackage;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Checking code commit in GIT");
+	}
+
+}
